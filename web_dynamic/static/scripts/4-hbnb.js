04f35b4953d7fb50dbf1301d34e425ed85a8ba1e
DaisyGeraldine <chipanal@hotmail.com>
2022-12-30 19:00:00 -0500

    fix : 100-hbnb.js, 101-hbnb.js, 3-hbnb.js, 4-hbnb.js <> change the text of the information Guests class to Bedrooms and Bathrooms

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -38,12 +38,12 @@ $(document).ready(function () {
         <article>
           <div class="title_box">
             <h2>${place.name}</h2>
-            <div class="price_by_night">${place.price_by_night}</div>
+            <div class="price_by_night">$${place.price_by_night}</div>
           </div>
           <div class="information">
             <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-            <div class="number_rooms">${place.number_rooms} Guest${place.number_rooms !== 1 ? 's' : ''}</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Guest${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
           </div>
           <div class="user">
           </div>
@@ -80,12 +80,12 @@ $(document).ready(function () {
           <article>
             <div class="title_box">
               <h2>${place.name}</h2>
-              <div class="price_by_night">${place.price_by_night}</div>
+              <div class="price_by_night">$${place.price_by_night}</div>
             </div>
             <div class="information">
               <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-              <div class="number_rooms">${place.number_rooms} Guest${place.number_rooms !== 1 ? 's' : ''}</div>
-              <div class="number_bathrooms">${place.number_bathrooms} Guest${place.number_bathrooms !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
             </div>
             <div class="user">
             </div>
